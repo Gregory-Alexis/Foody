987cc00ac4c7252ea0d898dc3d1b42df91b935d5
Westindiess <gregoryalexiss@gmail.com>
2022-09-25 17:34:27 +0200

    App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios, { AxiosResponse } from 'axios';
+import React, { useEffect, useState } from 'react';
+import Header from './components/Header/Header';
+import Main from './components/Main/Main';
 
-function App() {
+const App: React.FC = () => {
+  const [data, setData] = useState<any>([]);
+  const [ingredients, setIngredients] = useState<string>('');
+
+  const url = `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ingredients}&apiKey=${process.env.REACT_APP_DATA_API}`;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result: AxiosResponse = await axios.get(url);
+      setData(result.data);
+    };
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='p-3'>
+      <Header
+        ingredients={ingredients}
+        setIngredients={setIngredients}
+        setData={setData}
+        data={data}
+      />
+      <Main data={data} />
     </div>
   );
-}
+};
 
 export default App;
