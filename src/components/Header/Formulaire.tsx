6d01e5227c704ccd2b8ef22a9ee86bc89ebb121e
Westindiess <gregoryalexiss@gmail.com>
2022-09-26 21:55:58 +0200

    refactored Form

--- src/components/Header/Formulaire.tsx
+++ src/components/Header/Formulaire.tsx
@@ -8,29 +8,27 @@ interface Props {
   ingredients: string;
   setIngredients: React.Dispatch<React.SetStateAction<string>>;
   setData: React.Dispatch<React.SetStateAction<any>>;
-  data: [];
 }
 
-const Formulaire: React.FC<Props> = ({ ingredients, setIngredients, setData, data }) => {
+const Formulaire: React.FC<Props> = ({ ingredients, setIngredients, setData }) => {
   // Permet d'écrire dans la barre de recherche
   const handleSearch = (event: React.FormEvent) => {
     setIngredients((event.target as HTMLInputElement).value);
   };
+
   const isMounted: React.MutableRefObject<boolean> = useIsMounted();
+
   /*
    * Lors d'un submit, la fonction fera un appel api et,
     ira chercher les recettes à base de l'ingrédient inscrit dans la barre de recherche.
    */
   const submitHandler = async (event: React.FormEvent) => {
     event.preventDefault();
-    try {
-      if (isMounted) {
-        fetchData(`/recipes/findByIngredients?ingredients=${ingredients}`).then((data) =>
-          setData(data)
-        );
-      }
-    } catch (error: any) {
-      throw new Error(error.message);
+
+    if (isMounted) {
+      fetchData(`/recipes/findByIngredients?ingredients=${ingredients}`).then((data) =>
+        setData(data)
+      );
     }
   };
 
@@ -46,6 +44,7 @@ const Formulaire: React.FC<Props> = ({ ingredients, setIngredients, setData, dat
         onChange={handleSearch}
         placeholder='search ingredients... ex: Apple'
       />
+
       <button type='submit' className='absolute right-0 bg-blue-600 p-1 rounded-md'>
         <RestaurantMenu />
       </button>
