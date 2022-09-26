b2aa758b088c5549f4a119767db3280234f6ee3a
Westindiess <gregoryalexiss@gmail.com>
2022-09-26 21:56:36 +0200

    Add return type to fetchData function

--- src/utils/fetchData.tsx
+++ src/utils/fetchData.tsx
@@ -3,7 +3,7 @@ import axios, { AxiosResponse } from 'axios';
 const BASE_URL: string = `https://api.spoonacular.com`;
 
 // Récupère les données API
-export const fetchData = async (endpoint: string) => {
+export const fetchData = async (endpoint: string): Promise<object> => {
   try {
     const { data }: AxiosResponse = await axios.get(
       `${BASE_URL}${endpoint}&apiKey=${process.env.REACT_APP_DATA_API}`
