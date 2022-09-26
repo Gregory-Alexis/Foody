e9868a6f4f68490651974983f6feec6a0439be9b
Westindiess <gregoryalexiss@gmail.com>
2022-09-26 20:59:37 +0200

    destructur the data from the Api

--- src/utils/fetchData.tsx
+++ src/utils/fetchData.tsx
@@ -4,10 +4,10 @@ const BASE_URL: string = `https://api.spoonacular.com`;
 
 export const fetchData = async (endpoint: string) => {
   try {
-    const result: AxiosResponse = await axios.get(
+    const { data }: AxiosResponse = await axios.get(
       `${BASE_URL}${endpoint}&apiKey=${process.env.REACT_APP_DATA_API}`
     );
-    return result;
+    return data;
   } catch (error: any) {
     throw new Error(error.message);
   }
