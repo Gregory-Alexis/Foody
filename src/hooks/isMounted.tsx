8bb05a13dd699fedd73e46d2da559d96995b1123
Westindiess <gregoryalexiss@gmail.com>
2022-09-28 00:50:18 +0200

    add type to function & variable

--- src/hooks/isMounted.tsx
+++ src/hooks/isMounted.tsx
@@ -1,8 +1,8 @@
 import { useEffect, useRef } from 'react';
 
 // Vérifie que le composant qui sert à afficher les données est bien monté
-export const useIsMounted = () => {
-  const isMounted = useRef(false);
+export const useIsMounted = (): React.MutableRefObject<boolean> => {
+  const isMounted: React.MutableRefObject<boolean> = useRef(false);
 
   useEffect(() => {
     isMounted.current = true;
