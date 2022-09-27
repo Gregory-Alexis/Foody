21738e780a3f8a3785649029cfad098029d4b9da
Westindiess <gregoryalexiss@gmail.com>
2022-09-28 00:49:36 +0200

    recipe title

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Recipe } from '../../models/recipe';
 
 /**
  * Typage des données reçu au lancement de l'application
@@ -8,22 +9,16 @@ interface Props {
 }
 
 /**
- * Typage pour le paramètre "el" de la méthode .map
- */
-interface Elem {
-  id: number;
-  image: string;
-}
-
-/**
  * Affiche les données API
  */
 const Main: React.FC<Props> = ({ data }) => {
   return (
     <div className='grid grid-cols-2 gap-3 mt-4'>
-      {data.map((el: Elem) => (
+      {/*Typage pour le paramètre "el" de la méthode .map*/}
+      {data.map((el: Recipe) => (
         <div key={el.id}>
           <img src={el.image} alt='recipes' className='rounded-lg' />
+          <h1>{el.title}</h1>
         </div>
       ))}
     </div>
