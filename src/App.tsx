69dd98002c9ce7d51544136863baa519cb29817d
Westindiess <gregoryalexiss@gmail.com>
2022-09-29 12:26:50 +0200

    new background

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,15 @@
 import React, { useState } from 'react';
+
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
 
 const App: React.FC = () => {
   // data contient les données de l'API
-  const [data, setData] = useState<any>([]);
-  /**
-   * la variable ingredients permet de rechercher des recettes via la barre de recherche en
-   * entrant l'ingrédient avec lequel on voudrait cuisiner.
-   * Exemple: Le mot "Apple" recherchera toutes les recettes à base de pommes.
-   */
-  const [ingredients, setIngredients] = useState<string>('');
 
   return (
-    <div className='p-3'>
-      <Header ingredients={ingredients} setIngredients={setIngredients} setData={setData} />
-      <Main data={data} />
+    <div className='p-3 bg-pink-100'>
+      <Header />
+      <Main />
     </div>
   );
 };
